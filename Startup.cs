52c3be509cdbd9440d2ebc3709ca4ae7bdc0ac1c
Nitin Paladiya <nj9666@gmail.com>
2020-05-09 01:13:00 +0530

    NJ_CrossOrigin

--- Startup.cs
+++ Startup.cs
@@ -6,12 +6,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using ShopCart.Models;
+using Microsoft.AspNetCore.Mvc.Cors.Internal;
 
 namespace ShopCart
 {
@@ -27,13 +29,24 @@ namespace ShopCart
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add Cors
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
+
             String connectionString = Configuration.GetConnectionString("DefaultConnection");
             services.AddDbContext<DemoCratContext>(opt => opt.UseSqlServer(connectionString));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddMvc().AddJsonOptions(x => x.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects);
 
-
+            services.Configure<MvcOptions>(options =>
+            {
+                options.Filters.Add(new CorsAuthorizationFilterFactory("MyPolicy"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +62,9 @@ namespace ShopCart
             }
 
             app.UseHttpsRedirection();
+
+            // Enable Cors
+            app.UseCors("MyPolicy");
             app.UseMvc();
         }
     }
