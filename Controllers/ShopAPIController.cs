faf231cf0f14983ae316dbca9177d945c48a8485
Nitin Paladiya <nj9666@gmail.com>
2020-08-28 04:39:12 +0530

    final commit

--- Controllers/ShopAPIController.cs
+++ Controllers/ShopAPIController.cs
@@ -2879,7 +2879,7 @@ namespace ShopCart.Controllers
             return Ok(objHttpCommonResponse);
         }
 
-
+        //AdminMstr/login
         [HttpPost]
         [Route("Adimn/Login")]
         public dynamic Adimn_Login([FromBody]AdminMstr objAdminData)
@@ -2941,7 +2941,8 @@ namespace ShopCart.Controllers
             return Ok(objInput);
         }
 
-        [HttpPost]
+
+       [HttpPost]
         [Route("Category/Insert")]
         public dynamic Category_Insert([FromBody]CategoryMstr objCategoryData)
         {
@@ -4325,6 +4326,49 @@ namespace ShopCart.Controllers
             }
 
 
+
+            //objHttpCommonResponse.success = objInput.success;
+            //objHttpCommonResponse.message = objInput.message;
+            //objHttpCommonResponse.data = objInput.Data;
+            //objHttpCommonResponse.AuthToken = objInput.AuthToken;
+            return Ok(objInput);
+        }
+
+        [HttpGet]
+        [Route("Vender/Forget_pwd")]
+        public dynamic Vender_Forget_pwd([FromBody]VenderMstr objVenderData)
+        {
+            //InputData objInput = new InputData();
+            InputloginData objInput = new InputloginData();
+            try
+            {
+                if (!_dbContext.UserMstr.Where(p => p.Email == objVenderData.Email).Any())
+                {
+                    objInput.status = false;
+                    objInput.message = "This Email is not register!!";
+                    objInput.Data = null;
+                }
+                else
+                {
+                    VenderMstr user = _dbContext.VenderMstr.Where(p => p.Email == objVenderData.Email).FirstOrDefault();
+                    var mail_html = "<!DOCTYPE html><html><head><title></title><meta http-equiv='Content-Type' content='text/html; charset=utf-8' /><meta name='viewport' content='width=device-width, initial-scale=1'><meta http-equiv='X-UA-Compatible' content='IE=edge' /><style type='text/css'>@media screen{@font-face{font-family:'Lato';font-style:normal;font-weight:400;src:local('Lato Regular'), local('Lato-Regular'), url(https://fonts.gstatic.com/s/lato/v11/qIIYRU-oROkIk8vfvxw6QvesZW2xOQ-xsNqO47m55DA.woff) format('woff')}@font-face{font-family:'Lato';font-style:normal;font-weight:700;src:local('Lato Bold'), local('Lato-Bold'), url(https://fonts.gstatic.com/s/lato/v11/qdgUG4U09HnJwhYI-uK18wLUuEpTyoUstqEm5AMlJo4.woff) format('woff')}@font-face{font-family:'Lato';font-style:italic;font-weight:400;src:local('Lato Italic'), local('Lato-Italic'), url(https://fonts.gstatic.com/s/lato/v11/RYyZNoeFgb0l7W3Vu1aSWOvvDin1pK8aKteLpeZ5c0A.woff) format('woff')}@font-face{font-family:'Lato';font-style:italic;font-weight:700;src:local('Lato Bold Italic'), local('Lato-BoldItalic'), url(https://fonts.gstatic.com/s/lato/v11/HkF_qI1x_noxlxhrhMQYELO3LdcAZYWl9Si6vvxL-qU.woff) format('woff')}}body,table,td,a{-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%}table,td{mso-table-lspace:0pt;mso-table-rspace:0pt}img{-ms-interpolation-mode:bicubic}img{border:0;height:auto;line-height:100%;outline:none;text-decoration:none}table{border-collapse:collapse !important}body{height:100% !important;margin:0 !important;padding:0 !important;width:100% !important}a[x-apple-data-detectors]{color:inherit !important;text-decoration:none !important;font-size:inherit !important;font-family:inherit !important;font-weight:inherit !important;line-height:inherit !important}@media screen and (max-width:600px){h1{font-size:32px !important;line-height:32px !important}}div[style*='margin: 16px 0;']{margin:0 !important}</style></head><body style='background-color: #f4f4f4; margin: 0 !important; padding: 0 !important;'><div style='display: none; font-size: 1px; color: #fefefe; line-height: 1px; font-family: Lato, Helvetica, Arial, sans-serif; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden;'> We weren't able to process your latest payment. Mind checking things out?</div><table border='0' cellpadding='0' cellspacing='0' width='100%'><tr><td bgcolor='#ff857a' align='center'><table border='0' cellpadding='0' cellspacing='0' width='100%' style='max-width: 600px;'><tr><td align='center' valign='top' style='padding: 40px 10px 40px 10px;'> <img alt='Logo' src='https://i.ibb.co/n3YRSnw/logo.png' width='auto' style='display: block;width: 100%;max-width: 250px;min-width: 40px;font-family: Lato, Helvetica, Arial, sans-serif;color: #ffffff;font-size: 18px;' border='0'></td></tr></table></td></tr><tr><td bgcolor='#ff857a' align='center' style='padding: 0px 10px 0px 10px;'><table border='0' cellpadding='0' cellspacing='0' width='100%' style='max-width: 600px;'><tr><td bgcolor='#ffffff' align='center' valign='top' style='padding: 40px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: #111111; font-family: Lato, Helvetica, Arial, sans-serif; font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;'><h1 style='font-size: 48px; font-weight: 400; margin: 0;'>Get your Password!</h1></td></tr></table></td></tr><tr><td bgcolor='#f4f4f4' align='center' style='padding: 0px 10px 0px 10px;'><table border='0' cellpadding='0' cellspacing='0' width='100%' style='max-width: 600px;'><tr><td bgcolor='#ffffff' align='left' style='padding: 20px 30px 40px 30px; color: #666666; font-family: Lato, Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;'><p style='margin: 0;'>This forget password request is sent by your account if the sender is you and everything is good then go and login in Doorja.</p></td></tr><tr><td bgcolor='#fff1ec' align='left' style='padding: 24px 48px; color: #666666; font-family: Lato, Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px; border-radius: 4px; '><p> To keep your account secure, you need to update your password. Use the login credentials to login into Doorja as usual.</p><p style='color: #ff857a;''><b>Your Email : " + user.Email + "<br> Your password: " + user.Password + "</b></p> <a href=' https://localhost:44363/Login/' style='color: #ff857a; text-decoration: underline;'>Login now</a><p> If you think this email was sent in error, please contact our team by phone at +1 (800) 123-4567 or reply to this email directly. Thank you!</p></td></tr><tr><td bgcolor='#ffffff' align='left' style='padding: 30px 20px 30px; color: #666666; font-family: Lato, Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;'><p style='margin: 0;'>If you have any questions, just reply to this email—we're always happy to help out.</p></td></tr><tr><td bgcolor='#ffffff' align='left' style='padding: 0px 30px 40px 30px; border-radius: 0px 0px 4px 4px; color: #666666; font-family: Lato, Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;'><p style='margin: 0;'>Cheers,<br>The Doorja Team</p></td></tr></table></td></tr><tr><td bgcolor='#f4f4f4' align='center' style='padding: 30px 10px 0px 10px;'><table border='0' cellpadding='0' cellspacing='0' width='100%' style='max-width: 600px;'><tr><td bgcolor='#fff1ec' align='center' style='padding: 30px 30px 30px 30px; border-radius: 4px 4px 4px 4px; color: #666666; font-family: Lato, Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;'><h2 style='font-size: 20px; font-weight: 400; color: #111111; margin: 0;'>Need more help?</h2><p style='margin: 0;'><a href='javascript:void(0)' style='color: #ff857a;'>We&rsquo;re here, ready to talk</a></p></td></tr></table></td></tr><tr><td bgcolor='#f4f4f4' align='center' style='padding: 0px 10px 0px 10px;'><table border='0' cellpadding='0' cellspacing='0' width='100%' style='max-width: 600px;'><tr><td bgcolor='#f4f4f4' align='left' style='padding: 30px 30px 30px 30px; color: #666666; font-family: Lato, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 18px;'><p style='margin: 0;'> <a href='javascript:void(0)' style='color: #111111; font-weight: 700;'>Dashboard</a> - <a href='javascript:void(0)' style='color: #111111; font-weight: 700;'>Billing</a> - <a href='javascript:void(0)' style='color: #111111; font-weight: 700;'>Help</a></p></td></tr><tr><td bgcolor='#f4f4f4' align='left' style='padding: 0px 30px 30px 30px; color: #666666; font-family: Lato, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 18px;'><p style='margin: 0;'>You received this email because we had problems billing your account. If it looks weird, <a href='javascript:void(0)' style='color: #111111; font-weight: 700;'>view it in your browser</a>.</p></td></tr><tr><td bgcolor='#f4f4f4' align='left' style='padding: 0px 30px 30px 30px; color: #666666; font-family: Lato, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 18px;'><p style='margin: 0;'>If these emails get annoying, please feel free to <a href='javascript:void(0)' style='color: #111111; font-weight: 700;'>unsubscribe</a>.</p></td></tr><tr><td bgcolor='#f4f4f4' align='left' style='padding: 0px 30px 30px 30px; color: #666666; font-family: Lato, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 18px;'><p style='margin: 0;'>Ceej - 1234 Main Street - Anywhere, MA - 56789</p></td></tr></table></td></tr></table></body></html>";
+
+                    sendMail(user.Email, "Your Doorja login credentials", mail_html);
+                    List<VenderMstr> venders = new List<VenderMstr>();
+                    venders.Add(user);
+
+                    objInput.status = true;
+                    objInput.message = "We sent you email regarding your login credentials on this mail address \" " + objVenderData.Email + " \".";
+                    objInput.Data = venders;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                objInput.status = false;
+                objInput.message = "Something went wrong, please contact support";
+                objInput.Data = null;
+            }
 
             //objHttpCommonResponse.success = objInput.success;
             //objHttpCommonResponse.message = objInput.message;
